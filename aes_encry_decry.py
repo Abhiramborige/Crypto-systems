74b284b7d75eaae801e7ca6f2b2e6339f575e97d
Borige.Abhiram <54103320+Abhiramborige@users.noreply.github.com>
2020-08-17 21:02:12 +0530

    Done

---
+++ aes_encry_decry.py
@@ -0,0 +1,52 @@
+import hashlib
+from Crypto import Random
+from Crypto.Cipher import AES
+from base64 import b64encode, b64decode
+
+class AESCipher(object):
+    def __init__(self, key):
+        self.block_size = AES.block_size
+        self.key = hashlib.sha256(key.encode()).digest()
+
+    def encrypt(self, plain_text):
+        plain_text = self.__pad(plain_text)
+        iv = Random.new().read(self.block_size)
+        cipher = AES.new(self.key, AES.MODE_CBC, iv)
+        encrypted_text = cipher.encrypt(plain_text.encode())
+        return b64encode(iv + encrypted_text).decode("utf-8")
+
+    def decrypt(self, encrypted_text):
+        encrypted_text = b64decode(encrypted_text)
+        iv = encrypted_text[:self.block_size]
+        cipher = AES.new(self.key, AES.MODE_CBC, iv)
+        plain_text = cipher.decrypt(encrypted_text[self.block_size:]).decode("utf-8")
+        return self.__unpad(plain_text)
+
+    def __pad(self, plain_text):
+        number_of_bytes_to_pad = self.block_size - len(plain_text) % self.block_size
+        ascii_string = chr(number_of_bytes_to_pad)
+        padding_str = number_of_bytes_to_pad * ascii_string
+        padded_plain_text = plain_text + padding_str
+        print("The plain text after padding: ",padded_plain_text)
+        return padded_plain_text
+
+    @staticmethod
+    def __unpad(plain_text):
+        last_character = plain_text[len(plain_text) - 1:]
+        return plain_text[:-ord(last_character)]
+
+key=input("Enter the key: ")
+c=AESCipher(key)
+plain_text=input("Enter the message: ")
+print("The message is: ", plain_text)
+
+cipher=c.encrypt(plain_text)
+print("Encrypted message is: ",cipher)
+
+dec=c.decrypt(cipher)
+print("Decrypted message is: ",dec)
+
+'''
+Took help from: https://medium.com/quick-code/aes-implementation-in-python-a82f582f51c2
+Thanks for the help !!!
+'''
