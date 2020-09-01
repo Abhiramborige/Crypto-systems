8f8c89bfea361133cb936c109f91fac0877cfcff
Borige.Abhiram <54103320+Abhiramborige@users.noreply.github.com>
2020-09-01 09:22:29 +0530

    Done

--- diff_hellmen.py
+++ diff_hellmen.py
@@ -1,5 +1,4 @@
 
-
 print ("Both parties agree to a single prime")
 prime=int(input("Enter the prime number to be considered: "))
 
@@ -46,4 +45,35 @@ print ("Party2 calculates the shared key and gets", bobkey, "\n")
 #Both Alice and Bob now share a key which Eve cannot calculate
 print ("Attacker does not know the shared private key that Party1 & Party2 can now use")
 
+'''
+----------OUTPUT----------
+Both parties agree to a single prime
+Enter the prime number to be considered: 31
+Both must agree with single primitive root to use
+Enter the primitive root: 11
+Enter a secret number for Party1: 98
+Enter a secret number for Party2: 60
 
+
+Party1's  public key -> A = root ^ alicesecret mod prime
+Party1 public key is:  19 
+
+Party2's public key -> B = root ^ bobsecret mod prime
+Party2 public key is 1 
+
+Party1 calculates the shared key as K=B^alicesecret*(mod(prime))
+Party1 calculates the shared key and results:  1 
+
+Party2 calculates the shared key as K = A ^ bobsecret mod prime
+Party2 calculates the shared key and gets 1 
+
+Attacker does not know the shared private key that Party1 & Party2 can now use
+>>> 
+'''
+
+'''
+Took help from:
+1. https://sublimerobots.com/2015/01/simple-diffie-hellman-example-python/
+2. https://trinket.io/python/d574095364
+3. https://www.wolframalpha.com/widgets/view.jsp?id=ef51422db7db201ebc03c8800f41ba99
+'''
