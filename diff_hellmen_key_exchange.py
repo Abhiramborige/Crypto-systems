013ce395410bc85d0a3f1dd5174f56d01246b14d
Borige.Abhiram <54103320+Abhiramborige@users.noreply.github.com>
2020-09-03 14:35:52 +0530

    Done

--- diff_hellmen_key_exchange.py
+++ diff_hellmen_key_exchange.py
@@ -15,14 +15,14 @@ print("\n")
 
 # Party1 public key A=(root^alicesecret)*mod(prime)
 
-print ("Party1's  public key -> A = root ^ alicesecret mod prime")
+print ("Party1's  public key -> A = root^alicesecre*mod(prime))")
 alicepublic=(root**alicesecret)%prime
 print ("Party1 public key is: ",alicepublic, "\n")
 
 
 
 # Party2 public key B=(root^bobsecret)*mod(prime)
-print ("Party2's public key -> B = root ^ bobsecret mod prime")
+print ("Party2's public key -> B = root^bobsecret*)mod(prime))")
 bobpublic=(root**bobsecret)%prime
 print ("Party2 public key is", bobpublic, "\n")
 
@@ -38,7 +38,7 @@ print ("Party1 calculates the shared key and results: ",alicekey, "\n")
 
 # Party2 calculates the shared key K:
 # K = A^(bobsecret)*mod(prime)
-print ("Party2 calculates the shared key as K = A ^ bobsecret mod prime")
+print ("Party2 calculates the shared key as K = A^bobsecret*(mod(prime))")
 bobkey =(alicepublic**bobsecret)%prime
 print ("Party2 calculates the shared key and gets", bobkey, "\n")
 
@@ -55,16 +55,16 @@ Enter a secret number for Party1: 98
 Enter a secret number for Party2: 60
 
 
-Party1's  public key -> A = root ^ alicesecret mod prime
+Party1's  public key -> A = root^alicesecre*mod(prime))
 Party1 public key is:  19 
 
-Party2's public key -> B = root ^ bobsecret mod prime
+Party2's public key -> B = root^bobsecret*)mod(prime))
 Party2 public key is 1 
 
 Party1 calculates the shared key as K=B^alicesecret*(mod(prime))
 Party1 calculates the shared key and results:  1 
 
-Party2 calculates the shared key as K = A ^ bobsecret mod prime
+Party2 calculates the shared key as K = A^bobsecret*(mod(prime))
 Party2 calculates the shared key and gets 1 
 
 Attacker does not know the shared private key that Party1 & Party2 can now use
