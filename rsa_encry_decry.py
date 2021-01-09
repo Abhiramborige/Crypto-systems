cc4889dae871765bd5b73a66a6dc63113f5c6f82
__abhi__ <54103320+Abhiramborige@users.noreply.github.com>
2021-01-09 13:46:50 +0530

    Done ✔

--- rsa_encry_decry.py
+++ rsa_encry_decry.py
@@ -1,27 +1,38 @@
-from Crypto.Util.number import *
 import math
 import string
 
 main=string.ascii_lowercase
 
-# Euclid's extended algorithm for finding the multiplicative inverse of two numbers
+# Pseudo code for GCD is
+'''
+function gcd(a, b)
+    if b = 0
+        return a
+    else
+        return gcd(b, a mod b)
+'''
+
+# Naive method finding the multiplicative inverse of two numbers
 def multiplicative_inverse(a, m):
-    a = a % m; 
-    for x in range(1, m) : 
-        if ((a * x) % m == 1) : 
+    a=a%m; 
+    for x in range(1,m) : 
+        if((a*x)%m==1) : 
             return x 
     return 1
 
-# Fast modular exponentiation function
+# Fast modular exponentiation function (can be used when
+# something is very large for calculation modular)
+'''
 def get_mod_expo(base,exponent,modulus):
     result=1
     while exponent:
-        exponent=exponent//2
         d=exponent%2
+        exponent=exponent//2
         if d:
             result=result*base%modulus
         base=base*base%modulus
     return result
+'''
 
 # Function to generate a public and private key pair
 def generate_keypair(p, q):
@@ -38,6 +49,7 @@ def generate_keypair(p, q):
     e=int(input())
 
     # Using Euclid's Algorithm to verify that e and phi(n) are co-prime
+    # THe built in function gcd helps with the same
     g=math.gcd(e,phi)
     while(g!=1):
         print("The number you entered is not co-prime")
@@ -46,15 +58,19 @@ def generate_keypair(p, q):
         
     print("Value of exponent(e) entered is: ", e)
 
-    # Use Extended Euclid's Algorithm to generate the private key
+    # To generate the private key
     d = multiplicative_inverse(e, phi)
+    # We can use Extended Euclidean Algorithm because
+    # we know that e and phi are coprimes
+    
     # Public key is (e, n) and private key is (d, n)
     return (e,n),(d,n)
 
 # Function to Encrypt the message
 def encrypt(public_key, to_encrypt):
     key, n = public_key
-    
+
+    # we can also use fast modular exponentiation here
     cipher=pow(to_encrypt,key)%n
     return cipher
 
@@ -62,22 +78,23 @@ def encrypt(public_key, to_encrypt):
 # Function to Decrypt the message
 def decrypt(private_key, to_decrypt):
     key, n = private_key
-    
+
+    # we can also use fast modular exponentiation here
     decrypted=pow(to_decrypt,key)%n
     return decrypted
 
 # Main Program
 # primes of 8 bits in length in binary
-p=getPrime(8)
-q=getPrime(8)
+p=int(input("Enter prime p: "))
+q=int(input("Enter prime q (!=p): "))
 
 # to make sure that p not equal to q while generating randomly
 while(p==q):
-    p=getPrime(8)
-    q=getPrime(8)
+    p=int(input("Enter prime p: "))
+    q=int(input("Enter prime q (!=p): "))
     
-print("Randomly generated prime number p: ",p)
-print("Randomly generated prime number q: ",q)
+print("Prime number p: ",p)
+print("Prime number q: ",q)
 
 print("Generating Public/Private key-pairs!")
 public, private = generate_keypair(p, q)
@@ -111,8 +128,10 @@ print("Decrypted message (Plain Text): ", plain_text)
 
 '''
 ----------OUTPUT----------
-Randomly generated prime number p:  157
-Randomly generated prime number q:  149
+Enter prime p: 157
+Enter prime q (!=p): 149
+Prime number p:  157
+Prime number q:  149
 Generating Public/Private key-pairs!
 Value of n:  23393
 Value of phi(n):  23088
@@ -121,7 +140,7 @@ Enter e such that is co-prime to  23088 :
 Value of exponent(e) entered is:  23
 Your public key is (e,n)  (23, 23393)
 Your private key is (d,n)  (6023, 23393)
-Enter the message: WORLD WINNER
+Enter the message: world winner
 Encrypted message (Cipher Text):  [22344, 18436, 8290, 15143, 15339, 22344, 8926, 8780, 8780, 22321, 8290]
 Plain text array:  [22, 14, 17, 11, 3, 22, 8, 13, 13, 4, 17]
 Decrypted message (Plain Text):  worldwinner
@@ -132,5 +151,9 @@ Decrypted message (Plain Text):  worldwinner
 Took help from: 
 1. https://github.com/agottiparthy1/rsa/blob/master/rsa_python
 2. https://github.com/faisalkhan91/RSA-Algorithm/blob/master/RSA.py
+3. https://www.geeksforgeeks.org/multiplicative-inverse-under-modulo-m/
+4. https://www.geeksforgeeks.org/euclidean-algorithms-basic-and-extended/
+5. https://www.geeksforgeeks.org/modular-exponentiation-power-in-modular-arithmetic/
+6. https://www.khanacademy.org/computing/computer-science/cryptography#modarithmetic
 Thank you !!
 '''
